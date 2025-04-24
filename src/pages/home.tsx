a0820d315744f7cd36067ca8e9f1d2b5f2685096
tokujiTO <tiagomassuda123@gmail.com>
2025-04-24 12:27:13 -0300

    feat: add carousel component and start membersPage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,8 +6,6 @@ export default function Home() {
   return (
     <div className="pt-20">
       <Navbar />
-      <h1 className='text-3xl text-red-800 font-thin'>Home</h1>
-      <p>Bem-vindo à página inicial!</p>
       <Section1 />
       <Footer />
     </div>
