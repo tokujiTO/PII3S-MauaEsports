67a5c18de2e6d4521e76f652659fc986f83e177a
tokujiTO <tiagomassuda123@gmail.com>
2025-04-24 14:32:45 -0300

    feat: add all sections and responsiviness of navbar and footer fixed

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -1,14 +1,16 @@
 import Footer from "../components/footer"
 import Diretoria from "../components/Members/Diretoria"
 import Marketing from "../components/Members/Marketing"
+import Teams from "../components/Members/Teams"
 import Navbar from "../components/navbar"
 
 export default function Members() {
   return (
-    <div className='overflow-hidden pt-20'>
+    <div className='overflow-hidden pt-20 bg-deepBlue'>
       <Navbar />
       <Diretoria />
       <Marketing />
+      <Teams />
       <Footer />
     </div>
   )
