0fb654a8cae8c906010e63ef612ee302bd9e20ca
tokujiTO <tiagomassuda123@gmail.com>
2025-04-29 09:06:02 -0300

    feat: fix modal

--- src/components/Admin/campeonatosModal.tsx
+++ src/components/Admin/campeonatosModal.tsx
@@ -32,10 +32,21 @@ export default function CampeonatosModal({
       onClick={handleClose}
     >
       <div
-        className={`flex h-4/5 w-3/4 items-center justify-center rounded-3xl bg-white shadow-lg ${visible ? 'translate-y-0' : 'translate-y-full'} transition-transform duration-200`}
+        className={`flex h-4/5 w-3/4 flex-col items-start justify-start overflow-y-scroll rounded-3xl bg-white px-4 py-6 shadow-lg ${visible ? 'translate-y-0' : 'translate-y-full'} gap-4 transition-transform duration-200`}
         onClick={(e) => e.stopPropagation()}
       >
-        <h1>Campeonatos</h1>
+        <div className="flex w-full flex-col items-start justify-center">
+          <h1>Campeonatos</h1>
+          <div className="h-1 w-full rounded-full bg-black" />
+        </div>
+        <div className="flex w-full justify-end gap-6 text-2xl">
+          <button className="flex w-1/5 items-center justify-center rounded-xl bg-red-400 p-2 duration-200 hover:cursor-pointer hover:bg-red-600">
+            cancelar
+          </button>
+          <button className="flex w-1/5 items-center justify-center rounded-xl bg-blue-400 p-2 duration-200 hover:cursor-pointer hover:bg-blue-600">
+            salvar
+          </button>
+        </div>
       </div>
     </div>
   );
