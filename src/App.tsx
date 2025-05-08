d3183009f282702d7d73a1bbb56370308862b97b
Guilherme Alvarez <guilherme_alvarez18@hotmail.com>
2025-05-08 19:40:16 -0300

    eu fiz front-end????? meu deus!!! :O

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from './pages/notFound';
 import Tournments from './pages/tournments';
 import Login from './pages/admin/login';
 import HomeInterno from './pages/admin/homeInterno';
+import Cadastrar from './pages/admin/cadastrar';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/campeonatos" element={<Tournments />} />
         <Route path="/login" element={<Login />} />
         <Route path="/pi-home" element={<HomeInterno />} />
+        <Route path='/cadastrar' element={<Cadastrar/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
